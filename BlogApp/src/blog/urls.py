8e251d5b83571e37b90b8501a18f192501ab8b64
Bojan Jovanovic <bojanjovanovic@Bojans-MacBook-Pro.local>
2017-01-22 00:26:59 -0600

    Setup Static Files

--- BlogApp/src/blog/urls.py
+++ BlogApp/src/blog/urls.py
@@ -13,7 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.conf.urls import url, include
+from django.conf.urls.static import static
 from django.contrib import admin
 
 from posts.views import listView
@@ -22,3 +24,6 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include("posts.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
